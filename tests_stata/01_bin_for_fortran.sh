7ae0a3206faef5222b5a08b62feaeb8e49cd708b
Tiago Tavares <tgstavares@gmail.com>
2025-11-25 15:32:29 +0000

    almost done with testing

--- tests_stata/01_bin_for_fortran.sh
+++ tests_stata/01_bin_for_fortran.sh
@@ -4,17 +4,11 @@
 DIRPROGS="/home/tgst/Desktop/Project_estimator/Project_estimator"
 
 # Execute a python program located in that directory
-$DIRPROGS/tools/dataframe_to_fe_binary.py temp_nlsw_test.parquet \
-    --output temp_nlsw_test.bin \
+$DIRPROGS/tools/dataframe_to_fe_binary.py nlsw_test.parquet \
+    --output nlsw_test.bin \
     --y ln_wage \
-    --x hours,ttl_exp,union,tenure,wks_ue,gg2,gg3,gg4,gg5,gg6,gg7,gg8,gg9,gg10,gg11,gg12 \
+    --x hours,ttl_exp,union,tenure,wks_ue,msp \
+    --iv wks_work,ind_code \
     --fe idcode,occ_code,year \
     --drop-missing --summary --verbose --workers 16 
-
-# $DIRPROGS/tools/dataframe_to_fe_binary.py nlsw_test.parquet \
-#     --output nlsw_test.bin \
-#     --y ln_wage \
-#     --x hours,ttl_exp,union,tenure,wks_ue,ind_code,msp \
-#     --fe idcode,occ_code,year \
-#     --drop-missing --summary --verbose --workers 16 
 
